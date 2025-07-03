12709b09cbe14f1fde6acd16678594c0bf433a17
Vishal Shishodia <vishalshishodia@Vishals-MacBook-Air.local>
2025-07-03 14:22:55 +0530

    Add root route for health check

--- app/main.py
+++ app/main.py
@@ -5,6 +5,18 @@ from app.routes import ops, client
 from app.database import Base, engine
 import os
 import uvicorn
+from fastapi import FastAPI
+from app.routes import client_user, ops_user, file_access
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Secure File Sharing API is running!"}
+
+app.include_router(client_user.router)
+app.include_router(ops_user.router)
+app.include_router(file_access.router)
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=10000, reload=False)
